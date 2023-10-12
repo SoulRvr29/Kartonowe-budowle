11e03d8c88593708a95e017a5adc32493cf1afca
Paweł Chudecki <pawelc29@interia.pl>
2023-10-12 15:58:48 +0200

    gallery update

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -29,7 +29,7 @@ const Gallery = (id) => {
   }
 
   return (
-    <section className="px-8  ">
+    <section className="px-8  select-none">
       <h3 className="font-bold">
         {" "}
         <button
@@ -107,43 +107,37 @@ const Gallery = (id) => {
           })}
       </div>
 
-      {/* full size gallery */}
+      {/* FULL SIZE PHOTO GALLERY */}
       {fullPhotoState === true && (
         <div
           onClick={() => setFullPhotoState(false)}
           className="full-size fixed w-full h-full overflow-auto  bg-black top-0 left-0 z-30 bg-opacity-40 grid place-content-center"
         >
-          <button
-            className="grid"
-            onClick={(e) => {
-              e.stopPropagation();
-              setPhotoSize(!photoSize);
-            }}
-          >
-            {/* close icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="2em"
-              viewBox="0 0 384 512"
-              fill="white"
-              className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2  m-8"
-              onClick={(e) => {
-                e.stopPropagation();
-                setFullPhotoState(false);
-              }}
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-            {photoSize === false ? (
-              // left icon
-              <div className="grid">
-                {/* right icon */}
+          {/* ICON CONTAINER */}
+          <div className="w-full grid max-w-6xl place-self-center mx-auto h-full absolute">
+            {/* close icon medium*/}
+            {photoSize == false ? (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 384 512"
+                  fill="white"
+                  className="hover:cursor-pointer fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2  m-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setFullPhotoState(false);
+                  }}
+                >
+                  <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                </svg>
+                {/* left icon */}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="2em"
                   viewBox="0 0 320 512"
                   fill="white"
-                  className="fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 ml-8"
+                  className="hover:cursor-pointer fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 ml-8"
                   onClick={(e) => {
                     e.stopPropagation();
                     let nr = Number.parseInt(photoId);
@@ -154,12 +148,13 @@ const Gallery = (id) => {
                 >
                   <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
                 </svg>
+                {/* right icon */}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="2em"
                   viewBox="0 0 320 512"
                   fill="white"
-                  className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 mr-8"
+                  className="hover:cursor-pointer fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 mr-8"
                   onClick={(e) => {
                     e.stopPropagation();
                     let nr = Number.parseInt(photoId);
@@ -171,6 +166,71 @@ const Gallery = (id) => {
                 >
                   <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
                 </svg>
+              </>
+            ) : (
+              // close icon full
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 384 512"
+                  fill="white"
+                  className="hover:cursor-pointer fixed  hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 z-40 top-8 right-12"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setFullPhotoState(false);
+                  }}
+                >
+                  <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+                </svg>
+                {/* left icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="hover:cursor-pointer z-40 fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 left-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    if (nr > 1) nr--;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+                {/* right icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="hover:cursor-pointer z-40 fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 right-12"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    let max = galleryState === "day" ? dayMax : nightMax;
+                    if (nr < max) nr++;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+              </>
+            )}
+          </div>
+          <button
+            className="grid"
+            onClick={(e) => {
+              e.stopPropagation();
+              setPhotoSize(!photoSize);
+            }}
+          >
+            {/* MEDIUM WIDTH */}
+            {photoSize === false ? (
+              <div className="grid z-30">
                 <img
                   className="w-full max-w-4xl max-h-screen "
                   src={
@@ -182,42 +242,8 @@ const Gallery = (id) => {
                 />
               </div>
             ) : (
-              <div className="grid">
-                {/* left icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="2em"
-                  viewBox="0 0 320 512"
-                  fill="white"
-                  className="fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 ml-8"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    let nr = Number.parseInt(photoId);
-                    if (nr > 1) nr--;
-                    if (nr < 10) nr = "0" + nr;
-                    setPhotoId(nr);
-                  }}
-                >
-                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-                </svg>
-                {/* right icon */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="2em"
-                  viewBox="0 0 320 512"
-                  fill="white"
-                  className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 mr-8"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    let nr = Number.parseInt(photoId);
-                    let max = galleryState === "day" ? dayMax : nightMax;
-                    if (nr < max) nr++;
-                    if (nr < 10) nr = "0" + nr;
-                    setPhotoId(nr);
-                  }}
-                >
-                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-                </svg>
+              // FULL WIDTH
+              <div className="grid z-30">
                 <img
                   className="w-full "
                   src={
