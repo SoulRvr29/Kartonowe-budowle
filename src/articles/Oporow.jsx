3f985b6fb95fdfbd82ee6f8dfd5fc0bba0936c0b
Paweł Chudecki <pawelc29@interia.pl>
2023-10-12 15:08:56 +0200

    working gallery

--- src/articles/Oporow.jsx
+++ src/articles/Oporow.jsx
@@ -1,72 +1,87 @@
 import modelsData from "../data/models-data.json";
 import ArticleHeader from "../components/ArticleHeader";
 import Gallery from "../components/Gallery";
+import { useState } from "react";
 
 const Article = () => {
   const model = modelsData[1];
+  const [historyState, setHistoryState] = useState(false);
   return (
     <article className=" mb-8 text-text-light">
       <ArticleHeader id={1} />
       <Gallery id={1} />
-      <div className="p-8">
-        <img
-          className="pb-8 pr-8 float-left "
-          src={model.photos.real}
-          alt="Oporów"
-        />
-        <p className=" text-justify">{model.description}</p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere optio
-          amet temporibus tenetur id nisi voluptatum dolor, eveniet suscipit
-          quae in corrupti ipsam enim, asperiores deserunt dignissimos nemo sunt
-          minima?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dolore
-          doloribus fugiat ratione eaque saepe porro sed aspernatur soluta
-          tempora commodi beatae vel repellendus, consectetur a? Ex harum esse
-          cupiditate!
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-          similique illo nulla omnis, eaque odit nemo reiciendis reprehenderit
-          eveniet hic totam fugiat voluptatibus tenetur minus nesciunt expedita
-          iste quos ad?
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
-          numquam nihil doloremque, doloribus voluptas eius, itaque impedit
-          magni porro quam a corrupti dolor illo veritatis cupiditate
-          laboriosam? Asperiores, odit temporibus. Atque dolor suscipit, modi
-          distinctio provident cupiditate a repellat doloribus voluptate
-          perspiciatis aperiam consequuntur. Asperiores minima voluptas libero
-          quibusdam, ipsam error repellendus earum! Quas, distinctio saepe.
-          Cumque magnam quam qui. Maiores, tempore doloremque! Vero quaerat
-          mollitia harum nostrum culpa doloremque, accusantium impedit eveniet
-          explicabo excepturi asperiores similique, porro repellendus assumenda
-          animi. Ad dolorem libero rem asperiores voluptatem in non cupiditate.
-        </p>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
-          placeat voluptas aperiam voluptate cum doloremque pariatur porro,
-          minima consectetur similique est expedita autem repellat esse dicta
-          quis repellendus nostrum laboriosam. Reiciendis laudantium nemo
-          laborum vero corrupti quibusdam nostrum aliquid repellat similique
-          ullam et tempora optio, commodi molestias deleniti soluta. Aperiam
-          quam in eos iste quasi, delectus doloremque nobis corrupti quidem.
-          Fugiat quasi facere nihil officia velit minus exercitationem,
-          praesentium libero optio perferendis at eius ab voluptatem harum
-          commodi ipsa in itaque, atque eum reiciendis laboriosam aliquam
-          consectetur autem! Tempore, dolorum? Molestias, vero dicta. Saepe
-          placeat nam sed, ratione hic autem ipsam aut, libero reprehenderit
-          modi ad vitae molestias! Dolore cupiditate libero quibusdam nemo
-          quidem voluptates odio autem dolor officiis quasi. Ullam deserunt
-          voluptas minus quidem corporis expedita ipsum atque et debitis
-          inventore vitae asperiores non distinctio nam, cum, cupiditate,
-          delectus tempora. Suscipit sunt iure accusamus quae ipsa assumenda eum
-          maxime.
-        </p>
-      </div>
+      <header className="font-bold mx-8 ">
+        <button
+          onClick={() => setHistoryState(!historyState)}
+          className="hover:underline "
+        >
+          <h3> Historia</h3>
+        </button>
+
+        <hr />
+      </header>
+      {historyState === true && (
+        <div className="p-8">
+          <img
+            className="pb-8 pr-8 float-left "
+            src={model.photos.real}
+            alt="Oporów"
+          />
+          <p className=" text-justify">{model.description}</p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+            optio amet temporibus tenetur id nisi voluptatum dolor, eveniet
+            suscipit quae in corrupti ipsam enim, asperiores deserunt
+            dignissimos nemo sunt minima?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum
+            dolore doloribus fugiat ratione eaque saepe porro sed aspernatur
+            soluta tempora commodi beatae vel repellendus, consectetur a? Ex
+            harum esse cupiditate!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+            similique illo nulla omnis, eaque odit nemo reiciendis reprehenderit
+            eveniet hic totam fugiat voluptatibus tenetur minus nesciunt
+            expedita iste quos ad?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime,
+            numquam nihil doloremque, doloribus voluptas eius, itaque impedit
+            magni porro quam a corrupti dolor illo veritatis cupiditate
+            laboriosam? Asperiores, odit temporibus. Atque dolor suscipit, modi
+            distinctio provident cupiditate a repellat doloribus voluptate
+            perspiciatis aperiam consequuntur. Asperiores minima voluptas libero
+            quibusdam, ipsam error repellendus earum! Quas, distinctio saepe.
+            Cumque magnam quam qui. Maiores, tempore doloremque! Vero quaerat
+            mollitia harum nostrum culpa doloremque, accusantium impedit eveniet
+            explicabo excepturi asperiores similique, porro repellendus
+            assumenda animi. Ad dolorem libero rem asperiores voluptatem in non
+            cupiditate.
+          </p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
+            placeat voluptas aperiam voluptate cum doloremque pariatur porro,
+            minima consectetur similique est expedita autem repellat esse dicta
+            quis repellendus nostrum laboriosam. Reiciendis laudantium nemo
+            laborum vero corrupti quibusdam nostrum aliquid repellat similique
+            ullam et tempora optio, commodi molestias deleniti soluta. Aperiam
+            quam in eos iste quasi, delectus doloremque nobis corrupti quidem.
+            Fugiat quasi facere nihil officia velit minus exercitationem,
+            praesentium libero optio perferendis at eius ab voluptatem harum
+            commodi ipsa in itaque, atque eum reiciendis laboriosam aliquam
+            consectetur autem! Tempore, dolorum? Molestias, vero dicta. Saepe
+            placeat nam sed, ratione hic autem ipsam aut, libero reprehenderit
+            modi ad vitae molestias! Dolore cupiditate libero quibusdam nemo
+            quidem voluptates odio autem dolor officiis quasi. Ullam deserunt
+            voluptas minus quidem corporis expedita ipsum atque et debitis
+            inventore vitae asperiores non distinctio nam, cum, cupiditate,
+            delectus tempora. Suscipit sunt iure accusamus quae ipsa assumenda
+            eum maxime.
+          </p>
+        </div>
+      )}
     </article>
   );
 };
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -12,16 +12,20 @@ const Gallery = (id) => {
 
   const [photoSize, setPhotoSize] = useState(false);
   const [fullPhotoState, setFullPhotoState] = useState(false);
-  // const [photoId, setPhotoId] = useState(0);
+  const [photoId, setPhotoId] = useState(1);
 
   const [galleryState, setGalleryState] = useState("day");
 
   for (let i = 1; i <= dayMax; i++) {
-    dayPhotos.push(daySrc + i + ".jpg");
+    let nr = i;
+    if (i < 10) nr = "0" + i;
+    dayPhotos.push(daySrc + nr + ".jpg");
   }
 
   for (let i = 1; i <= nightMax; i++) {
-    nightPhotos.push(nightSrc + i + ".jpg");
+    let nr = i;
+    if (i < 10) nr = "0" + i;
+    nightPhotos.push(nightSrc + nr + ".jpg");
   }
 
   return (
@@ -58,18 +62,21 @@ const Gallery = (id) => {
 
       <hr />
       {/* thumbials */}
-      {galleryState === "day" && (
-        <div className="gallery relative flex gap-5 py-4 flex-wrap justify-center ">
-          {dayPhotos.map((loc) => {
+
+      <div className="gallery relative flex gap-5 py-4 flex-wrap justify-center ">
+        {galleryState === "day" &&
+          dayPhotos.map((loc) => {
             return (
               <button
-                key={dayPhotos.indexOf(loc) + 1}
-                onClick={() => {
+                key={loc}
+                onClick={(e) => {
                   setFullPhotoState(true);
+                  setPhotoId(e.target.attributes.value.value);
                 }}
                 className="z-10"
               >
                 <img
+                  value={loc.slice(-6, -4)}
                   className="h-[10rem] border-2 border-accent-3 rounded-xl hover:border-accent-2  hover:brightness-110 hover:scale-105 transition-all"
                   src={loc}
                   alt={model.name + " photo"}
@@ -77,17 +84,20 @@ const Gallery = (id) => {
               </button>
             );
           })}
-        </div>
-      )}
-      {galleryState === "night" && (
-        <div className="gallery h20 flex gap-5 p-4 flex-wrap justify-center">
-          {nightPhotos.map((loc) => {
+      </div>
+      <div className="gallery h20 flex gap-5 p-4 flex-wrap justify-center">
+        {galleryState === "night" &&
+          nightPhotos.map((loc) => {
             return (
               <button
-                key={dayPhotos.indexOf(loc) + 1}
-                onClick={() => setFullPhotoState(true)}
+                key={loc}
+                onClick={(e) => {
+                  setFullPhotoState(true);
+                  setPhotoId(e.target.attributes.value.value);
+                }}
               >
                 <img
+                  value={loc.slice(-6, -4)}
                   className="h-[10rem] border-2 border-accent rounded-xl hover:border-accent-2  hover:brightness-110 hover:scale-105 transition-all"
                   src={loc}
                   alt={model.name + " photo"}
@@ -95,32 +105,129 @@ const Gallery = (id) => {
               </button>
             );
           })}
-        </div>
-      )}
-      {/* full size */}
+      </div>
+
+      {/* full size gallery */}
       {fullPhotoState === true && (
         <div
           onClick={() => setFullPhotoState(false)}
-          className="fixed w-full h-full overflow-auto  bg-black top-0 left-0 z-30 bg-opacity-40 grid place-content-center"
+          className="full-size fixed w-full h-full overflow-auto  bg-black top-0 left-0 z-30 bg-opacity-40 grid place-content-center"
         >
           <button
+            className="grid"
             onClick={(e) => {
               e.stopPropagation();
               setPhotoSize(!photoSize);
             }}
           >
+            {/* close icon */}
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="2em"
+              viewBox="0 0 384 512"
+              fill="white"
+              className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2  m-8"
+              onClick={(e) => {
+                e.stopPropagation();
+                setFullPhotoState(false);
+              }}
+            >
+              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+            </svg>
             {photoSize === false ? (
-              <img
-                className="w-full max-w-4xl "
-                src={model.photos.day.full + "3.jpg"}
-                alt="photo"
-              />
+              // left icon
+              <div className="grid">
+                {/* right icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 ml-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    if (nr > 1) nr--;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 mr-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    let max = galleryState === "day" ? dayMax : nightMax;
+                    if (nr < max) nr++;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+                <img
+                  className="w-full max-w-4xl max-h-screen "
+                  src={
+                    galleryState === "day"
+                      ? model.photos.day.full + photoId + ".jpg"
+                      : model.photos.night.full + photoId + ".jpg"
+                  }
+                  alt={"photo" + photoId}
+                />
+              </div>
             ) : (
-              <img
-                className="w-full "
-                src={model.photos.day.full + "3.jpg"}
-                alt="photo"
-              />
+              <div className="grid">
+                {/* left icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="fixed hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 ml-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    if (nr > 1) nr--;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+                </svg>
+                {/* right icon */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="2em"
+                  viewBox="0 0 320 512"
+                  fill="white"
+                  className="fixed justify-self-end hover:bg-accent-2 rounded-full  aspect-square p-[2px] bg-accent drop-shadow-lg border-2 top-1/2 mr-8"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    let nr = Number.parseInt(photoId);
+                    let max = galleryState === "day" ? dayMax : nightMax;
+                    if (nr < max) nr++;
+                    if (nr < 10) nr = "0" + nr;
+                    setPhotoId(nr);
+                  }}
+                >
+                  <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+                </svg>
+                <img
+                  className="w-full "
+                  src={
+                    galleryState === "day"
+                      ? model.photos.day.full + photoId + ".jpg"
+                      : model.photos.night.full + photoId + ".jpg"
+                  }
+                  alt={"photo" + photoId}
+                />
+              </div>
             )}
           </button>
         </div>
