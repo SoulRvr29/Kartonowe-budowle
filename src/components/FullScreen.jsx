e333f5eae30244dfa41db73386d292f33dbc1bd0
Paweł Chudecki <pawelc29@interia.pl>
2023-10-21 15:06:33 +0200

    history full photo update

--- src/components/FullScreen.jsx
+++ src/components/FullScreen.jsx
@@ -4,65 +4,69 @@ const FullScreen = ({ prop, setFullScreen }) => {
   const [photoMaxWidth, setPhotoMaxWidth] = useState(false);
   const src = prop.target.currentSrc.match(/\bmodels.*\b/g);
   const title = prop.target.title;
-  console.log(prop);
+  document.querySelector("body").classList.add("site-overflow");
   return (
-    <div className="gallery relative flex gap-5 flex-wrap justify-center">
+    <div
+      onClick={() => {
+        setFullScreen(false);
+        document.querySelector("body").classList.remove("site-overflow");
+      }}
+      className="select-none grid full-size bg-black bg-opacity-70 backdrop-blur-sm fixed top-0 left-0 z-30 w-screen h-screen"
+    >
+      {/* ////////////  ICON CONTAINER  //////////// */}
       <div
-        onClick={() => {
-          setFullScreen(false);
-          document.querySelector("body").classList.remove("site-overflow");
-        }}
-        className="full-size fixed  w-full h-full overflow-auto bg-black top-0 left-0 z-30 bg-opacity-70 grid place-content-center backdrop-blur-sm gr-rows-1 max-sm:place-content-center"
+        className={
+          photoMaxWidth == false
+            ? "mx-auto relative grid max-w-6xl border w-full"
+            : "grid"
+        }
       >
-        {/* ////////////  ICON CONTAINER  //////////// */}
-        <div className="w-full grid max-w-6xl place-self-center mx-auto h-full absolute ">
-          {/* close icon */}
+        {/* close icon */}
 
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="3em"
-            viewBox="0 0 384 512"
-            fill="white"
-            className="fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 justify-self-end m-8"
-            onClick={(e) => {
-              e.stopPropagation();
-              setPhotoMaxWidth(false);
-              setFullScreen(false);
-              document.querySelector("body").classList.remove("site-overflow");
-            }}
-          >
-            <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-          </svg>
-        </div>
-        <button
-          className="grid "
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="3em"
+          viewBox="0 0 384 512"
+          fill="white"
+          className={
+            photoMaxWidth == false
+              ? "absolute hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 right-0 top-0 m-16 max-sm:justify-self-center max-sm:right-auto"
+              : "fixed opacity-50 hover:scale-125 hover:opacity-100 transition-all drop-shadow-[0_0_3px_grey] z-40 top-14 right-20 max-sm:top-2 max-sm:justify-self-center max-sm:right-auto"
+          }
+          onClick={(e) => {
+            e.stopPropagation();
+            setPhotoMaxWidth(false);
+            setFullScreen(false);
+            document.querySelector("body").classList.remove("site-overflow");
+          }}
+        >
+          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+        </svg>
+      </div>
+      {/* IMG CONTAINER medium-w-false / full-w-true */}
+      <div className="fixed w-screen h-screen left-0 top-0 z-30 grid place-content-center overflow-auto">
+        <img
           onClick={(e) => {
             e.stopPropagation();
             setPhotoMaxWidth(!photoMaxWidth);
           }}
+          className={
+            photoMaxWidth === false
+              ? "z-30 cursor-pointer medium-width border-4 border-accent-2 max-w-[500px] max-md:w-screen max-h-[95vh] max-sm:w-screen "
+              : "z-30 cursor-pointer medium-width border-4 border-accent-2 w-full overflow-auto  max-sm:min-w-max max-sm:w-screen"
+          }
+          src={src}
+          alt="photo"
+        />
+        <span
+          className={
+            photoMaxWidth === false
+              ? "photo-title opacity-0 text-lg max-w-[500px] bg-black px-3 border border-t-0 border-accent-2  w-fit max-sm:w-screen mx-auto text-center"
+              : "photo-title opacity-0  text-lg bg-black px-3 border border-accent-2 border-t-0 mx-auto text-center max-sm:fixed bottom-0 left-0 max-sm:border-t max-sm:w-screen max-sm:z-50"
+          }
         >
-          {/* IMG CONTAINER medium-w-false / full-w-true */}
-          <div className="medium-width grid z-30 mx-auto place-self-center ">
-            <img
-              className={
-                photoMaxWidth === false
-                  ? "w-full max-w-[500px] max-h-[85vh] border-4 border-accent-2 max-sm:w-screen "
-                  : "w-full border-4 border-accent-2  max-sm:w-max "
-              }
-              src={src}
-              alt="photo"
-            />
-            <span
-              className={
-                photoMaxWidth === false
-                  ? "photo-title opacity-0  text-lg max-w-[500px] bg-black px-3 border border-t-0 border-accent-2  w-fit max-sm:w-screen mx-auto"
-                  : "photo-title opacity-0  max-sm:fixed max-sm:bottom-0  max-sm:left-0 text-lg  max-sm:w-screen bg-black px-3 border border-accent-2 border-t-0 mx-auto"
-              }
-            >
-              {title}
-            </span>
-          </div>
-        </button>
+          {title}
+        </span>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,69 @@
 import { Link } from "react-router-dom";
 
 function App() {
+  let actualFontSize = 1;
+
+  const fontSizeChange = () => {
+    actualFontSize < 3 ? actualFontSize++ : (actualFontSize = 1);
+    actualFontSize === 1
+      ? (document.querySelector("body").style = "font-size: 1rem")
+      : actualFontSize === 2
+      ? (document.querySelector("body").style = "font-size: 1.1rem")
+      : (document.querySelector("body").style = "font-size: 1.2rem");
+  };
   return (
-    <header className="relative">
-      {/* left side */}
-      <div className="absolute flex h-full ml-4">
-        <button>
-          <Link to="/Kartonowe-budowle/" reloadDocument>
-            <svg
-              className="h-5 hover:fill-accent-2 transition-all max-sm:h-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 576 512"
-              fill="var(--text-light)"
-            >
-              <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
-            </svg>
-          </Link>
-        </button>
-      </div>
-      {/* center */}
-      <div className="flex gap-2 justify-center align-middle place-items-center">
-        <h1 className="text-accent text-header font-bold uppercase p-1 max-md:p-0">
-          Kartonowe &nbsp;budowle
-        </h1>
-      </div>
-      {/* right side */}
-      <div className="absolute flex gap-4 h-full right-0 top-0 mr-4">
-        {/* <button>
+    <>
+      <header className="relative mx-4 flex justify-between items-center">
+        {/* left side */}
+        <div className="mt-1">
+          <button>
+            <Link to="/Kartonowe-budowle/" reloadDocument>
+              <svg
+                className="hover:fill-accent-2 transition-all max-sm:h-4"
+                height="1.25em"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+                fill="var(--text-dark)"
+              >
+                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+              </svg>
+            </Link>
+          </button>
+        </div>
+        {/* center */}
+        <div className="flex gap-2 justify-center align-middle place-items-center">
+          <h1 className="text-accent text-header font-bold uppercase p-1 max-md:p-0">
+            Kartonowe &nbsp;budowle
+          </h1>
+        </div>
+        {/* right side */}
+        <div className="flex gap-2 mt-1">
           <svg
-            className="h-5 hover:fill-accent transition-all place-self-center"
-            fill="var(--text-light)"
+            className="hover:fill-accent-2 transition-all  max-sm:h-[14px] mt-[1px]"
             xmlns="http://www.w3.org/2000/svg"
+            height="1.1em"
             viewBox="0 0 448 512"
+            fill="var(--text-dark)"
+            onClick={() => fontSizeChange()}
           >
             <path d="M254 52.8C249.3 40.3 237.3 32 224 32s-25.3 8.3-30 20.8L57.8 416H32c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32h-1.8l18-48H303.8l18 48H320c-17.7 0-32 14.3-32 32s14.3 32 32 32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H390.2L254 52.8zM279.8 304H168.2L224 155.1 279.8 304z" />
           </svg>
-        </button> */}
-        {/* <button>
-          <svg
-            className="h-5 hover:fill-accent transition-all mb-1 place-self-center"
-            fill="var(--text-light)"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 192 512"
-          >
-            <path d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z" />
-          </svg>
-        </button> */}
-      </div>
-      {/* horizontal rule */}
-      <div className="grad-hr relative">
-        {/* <div className="flash absolute left-0 -top-[1px] h-[3px] aspect-square rounded-full blur-[1px] shadow-[0_0_5px_1px_white] bg-white z-50"></div> */}
-      </div>
-    </header>
+          <a href="mailto:pawelc29@interia.pl">
+            <svg
+              className="hover:fill-accent-2 transition-all  max-sm:h-4"
+              xmlns="http://www.w3.org/2000/svg"
+              height="1.25em"
+              viewBox="0 0 512 512"
+              fill="var(--text-dark)"
+            >
+              <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
+            </svg>
+          </a>
+        </div>
+        {/* horizontal rule */}
+      </header>
+      <div className="grad-hr relative"></div>
+    </>
   );
 }
 
