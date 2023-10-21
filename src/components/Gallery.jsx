ecd2dd2dd33b5237cc2ef70d5c914fe5401a3508
Paweł Chudecki <pawelc29@interia.pl>
2023-10-21 12:04:01 +0200

    gallery fix

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -53,7 +53,7 @@ const Gallery = ({ id }) => {
   };
 
   return (
-    <section className="gallery-cont px-8 select-none max-sm:px-4">
+    <section className="gallery-cont px-8 select-none max-sm:px-4 grid">
       <h3 className="font-bold flex flex-wrap text-2xl max-md:text-lg max-[400px]:text-base">
         {/* //////////// gallery on/of button //////////// */}
         <button
@@ -134,107 +134,109 @@ const Gallery = ({ id }) => {
       </div>
       {/* ////////////  FULL SIZE PHOTO GALLERY  //////////// */}
       {fullScreen === true && (
-        <div
-          onClick={() => {
-            setFullScreen(false);
-            document.querySelector("body").classList.remove("site-overflow");
-          }}
-          className="full-size fixed  w-full h-full overflow-auto bg-black top-0 left-0 z-30 bg-opacity-70 grid place-content-center backdrop-blur-sm grid-rows-1 max-sm:place-content-start"
-        >
-          {/* ////////////  ICON CONTAINER  //////////// */}
-          <div className="w-full grid max-w-6xl place-self-center mx-auto h-full absolute ">
-            {/* close icon */}
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="3em"
-              viewBox="0 0 384 512"
-              fill="white"
-              className={
-                photoMaxWidth == false
-                  ? "fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 justify-self-end m-8 max-sm:justify-self-center"
-                  : "fixed hover:cursor-pointer opacity-50 hover:scale-125 hover:opacity-100 transition-all drop-shadow-[0_0_3px_grey] z-40 top-14 right-20 max-sm:top-2 max-sm:right-3"
-              }
-              onClick={(e) => {
-                e.stopPropagation();
-                setPhotoMaxWidth(false);
-                setFullScreen(false);
-                document
-                  .querySelector("body")
-                  .classList.remove("site-overflow");
-              }}
-            >
-              <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
-            </svg>
-            {/* left icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="3em"
-              viewBox="0 0 320 512"
-              fill="white"
-              className={
-                photoMaxWidth == false
-                  ? "gallery-icon icon-hidden fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 top-1/2 ml-8 max-sm:top-auto max-sm:bottom-8"
-                  : "fixed hover:cursor-pointer opacity-50 hover:opacity-100 hover:scale-125 transition-all drop-shadow-[0_0_3px_grey] z-40 top-1/2 left-16 max-sm:left-4 "
-              }
-              onClick={(e) => {
-                e.stopPropagation();
-                imgSwitchHandler("left");
-              }}
-            >
-              <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
-            </svg>
-
-            {/* right icon */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="3em"
-              viewBox="0 0 320 512"
-              fill="white"
-              className={
-                photoMaxWidth == false
-                  ? "fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 justify-self-end top-1/2 mr-8 max-sm:top-auto max-sm:bottom-8"
-                  : "fixed hover:cursor-pointer opacity-50 hover:scale-125  hover:opacity-100 transition-all drop-shadow-[0_0_3px_grey] z-40 top-1/2 right-20 max-sm:right-4"
-              }
-              onClick={(e) => {
-                e.stopPropagation();
-                imgSwitchHandler("right");
-              }}
-            >
-              <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
-            </svg>
-          </div>
-          <button
-            className="grid max-sm:w-max"
-            onClick={(e) => {
-              e.stopPropagation();
-              setPhotoMaxWidth(!photoMaxWidth);
+        <section>
+          <div
+            onClick={() => {
+              setFullScreen(false);
+              document.querySelector("body").classList.remove("site-overflow");
             }}
+            className="full-size bg-black bg-opacity-70 backdrop-blur-sm fixed top-0 left-0 z-30 w-screen h-screen"
           >
+            {/* ////////////  ICON CONTAINER  //////////// */}
+            <div
+              className={
+                photoMaxWidth == false
+                  ? "mx-auto relative w-full h-full left-0 top-0 grid place-content-center max-w-6xl"
+                  : "w-full h-full left-0 top-0 grid place-content-center "
+              }
+            >
+              {/* close icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="3em"
+                viewBox="0 0 384 512"
+                fill="white"
+                className={
+                  photoMaxWidth == false
+                    ? "absolute hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 right-0 top-0 m-8 max-sm:justify-self-center"
+                    : "fixed hover:cursor-pointer opacity-50 hover:scale-125 hover:opacity-100 transition-all drop-shadow-[0_0_3px_grey] z-40 top-14 right-20 max-sm:top-2 max-sm:right-3"
+                }
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setPhotoMaxWidth(false);
+                  setFullScreen(false);
+                  document
+                    .querySelector("body")
+                    .classList.remove("site-overflow");
+                }}
+              >
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+              </svg>
+              {/* left icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="3em"
+                viewBox="0 0 320 512"
+                fill="white"
+                className={
+                  photoMaxWidth == false
+                    ? "gallery-icon icon-hidden fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 top-1/2 ml-8 max-sm:top-auto max-sm:bottom-8"
+                    : "fixed hover:cursor-pointer opacity-50 hover:opacity-100 hover:scale-125 transition-all drop-shadow-[0_0_3px_grey] z-40 top-1/2 left-16 max-sm:left-4 "
+                }
+                onClick={(e) => {
+                  e.stopPropagation();
+                  imgSwitchHandler("left");
+                }}
+              >
+                <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z" />
+              </svg>
+              {/* right icon */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="3em"
+                viewBox="0 0 320 512"
+                fill="white"
+                className={
+                  photoMaxWidth == false
+                    ? "fixed hover:cursor-pointer hover:scale-125 transition-all drop-shadow-[0_0_3px_black] z-40 justify-self-end top-1/2  mr-8 max-sm:top-auto max-sm:bottom-8"
+                    : "fixed hover:cursor-pointer opacity-50 hover:scale-125  hover:opacity-100 transition-all drop-shadow-[0_0_3px_grey] z-40 top-1/2 right-20 max-sm:right-4"
+                }
+                onClick={(e) => {
+                  e.stopPropagation();
+                  imgSwitchHandler("right");
+                }}
+              >
+                <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z" />
+              </svg>
+            </div>
             {/* IMG CONTAINER medium-w-false / full-w-true */}
-            <div className="medium-width grid z-30 mx-auto place-self-center ">
+            <div className="fixed w-screen h-screen left-0 top-0 z-30 grid place-content-center overflow-auto">
               <img
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setPhotoMaxWidth(!photoMaxWidth);
+                }}
                 className={
                   photoMaxWidth === false
-                    ? "w-full max-w-4xl max-h-[85vh] border-4 border-accent-2 max-sm:w-screen"
-                    : "w-full max-sm:h-screen max-sm:w-auto "
+                    ? "z-30 cursor-pointer medium-width border-4 border-accent-2 max-w-4xl max-md:w-screen max-h-[85vh] max-sm:w-screen "
+                    : "z-30 cursor-pointer medium-width w-screen overflow-auto max-sm:h-screen max-sm:min-w-max"
                 }
                 src={model.gallery[actualSection].full + photoId + ".jpg"}
                 alt={"photo" + photoId}
               />
-              {/* photo number */}
-              <span
-                className={
-                  photoMaxWidth === false
-                    ? "fixed bottom-4 text-2xl max-sm:text-xl drop-shadow-[0_0_2px_black] left-[calc(50%-38px)] z-40 border-2 px-2 pb-1 rounded-xl border-accent-3 text-accent-3 max-sm:top-auto max-sm:bottom-9"
-                    : "fixed bottom-4 text-3xl max-sm:text-xl drop-shadow-[0_0_2px_black] left-[calc(50%-48px)] max-sm:left-[calc(50%-38px)] z-40 border-2 px-2 pb-1 rounded-xl border-accent-3 text-accent-3 opacity-50 max-sm:top-auto max-sm:bottom-4"
-                }
-              >
-                {(photoId < 10 ? photoId[1] : photoId) + " / " + galleryLength}
-              </span>
             </div>
-          </button>
-        </div>
+          </div>
+          {/* photo number */}
+          <span
+            className={
+              photoMaxWidth === false
+                ? "fixed bottom-4 text-2xl max-sm:text-xl drop-shadow-[0_0_2px_black] left-[calc(50%-38px)] z-40 border-2 px-2 pb-1 rounded-xl border-accent-3 text-accent-3 max-sm:top-auto max-sm:bottom-9"
+                : "fixed bottom-4 text-3xl max-sm:text-xl drop-shadow-[0_0_2px_black] left-[calc(50%-48px)] max-sm:left-[calc(50%-38px)] z-40 border-2 px-2 pb-1 rounded-xl border-accent-3 text-accent-3 opacity-50 max-sm:top-auto max-sm:bottom-4"
+            }
+          >
+            {(photoId < 10 ? photoId[1] : photoId) + " / " + galleryLength}
+          </span>
+        </section>
       )}
     </section>
   );
