ae8c2dd2a382a057f6b8ef7ab7de7c1e3260908e
Paweł Chudecki <pawelc29@interia.pl>
2023-10-30 14:46:20 +0100

    settings save update

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -15,27 +15,27 @@ const Settings = ({
   const [confirm, setconfirm] = useState(false);
   return (
     <>
-      <div className="max-sm:grid hidden w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-30 z-20 backdrop-blur-sm"></div>
-      <div className="settings grid w-80 max-sm:right-auto max-sm:mx-4 max-sm:w-[calc(100%-2rem)] max-sm:top-7 select-none  absolute top-[62px] right-8 dark:bg-text-dark bg-text-light rounded-xl drop-shadow-[5px_5px_10px_rgba(0,0,0,0.5)] border-2 z-20 px-4 pt-1 pb-4 dark:text-text-light text-text-dark">
+      <div className="max-sm:grid w-screen h-screen fixed top-0 left-0 bg-black bg-opacity-30 z-20 "></div>
+      <div className="settings grid w-80 max-sm:right-auto max-sm:mx-4 max-sm:w-[calc(100%-2rem)] max-sm:top-7 select-none absolute top-[62px] right-8 dark:bg-text-dark bg-text-light rounded-xl drop-shadow-[5px_5px_10px_rgba(0,0,0,0.5)] border-2 dark:border-text-light dark:border-opacity-40 z-20 px-4 pt-1 pb-4 dark:text-text-light text-text-dark">
         <div className={!confirm ? " visible" : " invisible"}>
           <h5 className="text-center text-xl">Ustawienia</h5>
-          <hr className="bg-white h-[2px]" />
+          {/* <hr className="bg-white h-[2px]" /> */}
           <ul className=" font-medium grid gap-y-1 mt-2">
             {/* KOLORYSTYKA */}
-            <li className=" flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
+            <li className="rounded-md  flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
               Kolorystyka:{" "}
               <button
-                className="dark:hover:text-accent-2 hover:text-accent-4 transition-colors"
+                className="border-2 dark:border-text-dark w-24 rounded-lg px-2 dark:hover:text-text-dark dark:hover:bg-text-light hover:bg-text-dark hover:text-text-light transition-colors"
                 onClick={() => modeChange()}
               >
                 {darkMode ? "ciemna" : "jasna"}
               </button>
             </li>
             {/* WIELKOŚĆ CZCIONKI */}
-            <li className=" flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
+            <li className="rounded-md  flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
               Wielkość czcionki:{" "}
               <button
-                className="dark:hover:text-accent-2 hover:text-accent-4 transition-colors"
+                className="border-2 dark:border-text-dark w-24 rounded-lg px-2 dark:hover:text-text-dark dark:hover:bg-text-light hover:bg-text-dark hover:text-text-light transition-colors"
                 onClick={() => fontSize()}
               >
                 {actualFontSize == 1
@@ -46,20 +46,20 @@ const Settings = ({
               </button>
             </li>
             {/* RUCHOMY BANNER */}
-            <li className=" flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
+            <li className="rounded-md  flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
               Ruchomy banner:{" "}
               <button
-                className="dark:hover:text-accent-2 hover:text-accent-4 transition-colors"
+                className="border-2 dark:border-text-dark w-24 rounded-lg px-2 dark:hover:text-text-dark dark:hover:bg-text-light hover:bg-text-dark hover:text-text-light transition-colors"
                 onClick={() => setBannerState(!bannerState)}
               >
                 {bannerState ? "widoczny" : "ukryty"}
               </button>
             </li>
             {/* KARTY NAWIGACJI */}
-            <li className="max-sm:hidden flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
+            <li className="rounded-md max-sm:hidden flex justify-between bg-text-dark bg-opacity-20 p-1 px-2 dark:bg-text-light dark:bg-opacity-20">
               Karty nawigacji:{" "}
               <button
-                className="dark:hover:text-accent-2 hover:text-accent-4 transition-colors"
+                className="border-2 dark:border-text-dark w-24 rounded-lg px-2 dark:hover:text-text-dark dark:hover:bg-text-light hover:bg-text-dark hover:text-text-light transition-colors"
                 onClick={() => setOverlap(!overlap)}
               >
                 {overlap ? "rozsunięte" : "zsunięte"}
@@ -76,13 +76,13 @@ const Settings = ({
                     //   setSettingsState(false);
                   }, 1000);
                 }}
-                className=" drop-shadow-md bg-green-700 py-1 px-4 pb-[6px] hover:brightness-125 text-lg rounded-lg"
+                className="drop-shadow-lg bg-green-700 py-1 px-4 pb-[6px] hover:brightness-125 text-lg rounded-lg"
               >
                 Zapisz
               </button>
               <button
                 onClick={() => setSettingsState(false)}
-                className="drop-shadow-md  bg-accent-4 py-1 px-4 pb-[6px] hover:brightness-125 text-lg rounded-lg"
+                className="drop-shadow-lg  bg-accent-4 py-1 px-4 pb-[6px] hover:brightness-125 text-lg rounded-lg"
               >
                 Zamknij
               </button>
