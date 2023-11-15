522e243a8f968eb47c00cb2ef7c6683158c63673
Paweł Chudecki <pawelc29@interia.pl>
2023-11-15 22:39:22 +0100

    background fixed

--- src/App.jsx
+++ src/App.jsx
@@ -49,107 +49,115 @@ function App() {
     JSON.parse(localStorage.getItem("banner"))
   );
   return (
-    <main className=" relative bg-bkg-light dark:bg-bkg min-h-screen  flex flex-col max-w-6xl mx-auto border-text-light dark:border-opacity-70 dark:border-accent max-[1150px]:border-none border-x-2 border-opacity-30  text-text-dark dark:text-text-light  font-medium dark:font-normal ">
-      {/* <div className="w-full mx-auto h-full max-w-6xl top-0 left-0 absolute self-center z-[2]"></div> */}
-      <div className="dark:bg-accent bg-text-dark  w-full  mx-auto h-full top-0 left-0 absolute blur-3xl opacity-40 self-center -z-10"></div>
-      <Header
-        bannerState={bannerState}
-        setBannerState={setBannerState}
-        overlap={overlap}
-        setOverlap={setOverlap}
-      />
-      <Banner bannerState={bannerState} setBannerState={setBannerState} />
-      <Nav overlap={overlap} setOverlap={setOverlap} />
-      <BackToTop />
-      <Routes>
-        <Route path="/Kartonowe-budowle/" element={<Home />} />
-        <Route path="/Kartonowe-budowle/Bedzin" element={<Bedzin id={0} />} />
-        <Route path="/Kartonowe-budowle/Oporow" element={<Oporow id={1} />} />
-        <Route
-          path="/Kartonowe-budowle/Sandomierz"
-          element={<Sandomierz id={4} />}
+    <>
+      <div className="bkg-texture fixed top-0 left-0 w-screen h-screen flex justify-center"></div>
+      <main className=" relative bg-bkg-light dark:bg-bkg min-h-screen  flex flex-col max-w-6xl mx-auto border-text-light dark:border-opacity-70 dark:border-accent max-[1150px]:border-none border-x-2 border-opacity-30  text-text-dark dark:text-text-light  font-medium dark:font-normal ">
+        <div className="dark:bg-accent bg-text-dark  w-full  mx-auto h-full top-0 left-0 absolute blur-3xl opacity-40 self-center -z-10"></div>
+        <Header
+          bannerState={bannerState}
+          setBannerState={setBannerState}
+          overlap={overlap}
+          setOverlap={setOverlap}
         />
-        <Route path="/Kartonowe-budowle/Stilo" element={<Stilo id={2} />} />
-        <Route path="/Kartonowe-budowle/Darlowo" element={<Darlowo id={5} />} />
-        <Route
-          path="/Kartonowe-budowle/Czołpino"
-          element={<Czołpino id={6} />}
-        />
-        <Route path="/Kartonowe-budowle/Gąski" element={<Gąski id={8} />} />
-        <Route path="/Kartonowe-budowle/Hel" element={<Hel id={9} />} />
-        <Route
-          path="/Kartonowe-budowle/Jarosławiec"
-          element={<Jarosławiec id={10} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Jastarnia"
-          element={<Jastarnia id={11} />}
-        />
-        <Route path="/Kartonowe-budowle/Kikut" element={<Kikut id={12} />} />
-        <Route
-          path="/Kartonowe-budowle/Kołobrzeg"
-          element={<Kołobrzeg id={13} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Krynica Morska"
-          element={<KrynicaMorska id={14} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Niechorze"
-          element={<Niechorze id={15} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Nowy Port"
-          element={<NowyPort id={16} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Port Północny"
-          element={<PortPółnocny id={17} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Rozewie"
-          element={<Rozewie id={18} />}
-        />
-        <Route path="/Kartonowe-budowle/Sopot" element={<Sopot id={19} />} />
-        <Route
-          path="/Kartonowe-budowle/Świnoujście"
-          element={<Świnoujście id={20} />}
-        />
-        <Route path="/Kartonowe-budowle/Ustka" element={<Ustka id={7} />} />
-        <Route
-          path="/Kartonowe-budowle/Curiosity"
-          element={<Curiosity id={3} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Baranów"
-          element={<Baranów id={21} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Gryfino"
-          element={<Gryfino id={22} />}
-        />
-        <Route path="/Kartonowe-budowle/Mauler" element={<Mauler id={23} />} />
-        <Route
-          path="/Kartonowe-budowle/Predator"
-          element={<Predator id={24} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/PhantomF4"
-          element={<PhantomF4 id={25} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/StarshipSN15"
-          element={<StarshipSN15 id={26} />}
-        />
-        <Route
-          path="/Kartonowe-budowle/Falcon9"
-          element={<Falcon9 id={27} />}
-        />
-      </Routes>
-      <br />
-      <br />
-      <Footer />
-    </main>
+        <Banner bannerState={bannerState} setBannerState={setBannerState} />
+        <Nav overlap={overlap} setOverlap={setOverlap} />
+        <BackToTop />
+        <Routes>
+          <Route path="/Kartonowe-budowle/" element={<Home />} />
+          <Route path="/Kartonowe-budowle/Bedzin" element={<Bedzin id={0} />} />
+          <Route path="/Kartonowe-budowle/Oporow" element={<Oporow id={1} />} />
+          <Route
+            path="/Kartonowe-budowle/Sandomierz"
+            element={<Sandomierz id={4} />}
+          />
+          <Route path="/Kartonowe-budowle/Stilo" element={<Stilo id={2} />} />
+          <Route
+            path="/Kartonowe-budowle/Darlowo"
+            element={<Darlowo id={5} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Czołpino"
+            element={<Czołpino id={6} />}
+          />
+          <Route path="/Kartonowe-budowle/Gąski" element={<Gąski id={8} />} />
+          <Route path="/Kartonowe-budowle/Hel" element={<Hel id={9} />} />
+          <Route
+            path="/Kartonowe-budowle/Jarosławiec"
+            element={<Jarosławiec id={10} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Jastarnia"
+            element={<Jastarnia id={11} />}
+          />
+          <Route path="/Kartonowe-budowle/Kikut" element={<Kikut id={12} />} />
+          <Route
+            path="/Kartonowe-budowle/Kołobrzeg"
+            element={<Kołobrzeg id={13} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Krynica Morska"
+            element={<KrynicaMorska id={14} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Niechorze"
+            element={<Niechorze id={15} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Nowy Port"
+            element={<NowyPort id={16} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Port Północny"
+            element={<PortPółnocny id={17} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Rozewie"
+            element={<Rozewie id={18} />}
+          />
+          <Route path="/Kartonowe-budowle/Sopot" element={<Sopot id={19} />} />
+          <Route
+            path="/Kartonowe-budowle/Świnoujście"
+            element={<Świnoujście id={20} />}
+          />
+          <Route path="/Kartonowe-budowle/Ustka" element={<Ustka id={7} />} />
+          <Route
+            path="/Kartonowe-budowle/Curiosity"
+            element={<Curiosity id={3} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Baranów"
+            element={<Baranów id={21} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Gryfino"
+            element={<Gryfino id={22} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Mauler"
+            element={<Mauler id={23} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Predator"
+            element={<Predator id={24} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/PhantomF4"
+            element={<PhantomF4 id={25} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/StarshipSN15"
+            element={<StarshipSN15 id={26} />}
+          />
+          <Route
+            path="/Kartonowe-budowle/Falcon9"
+            element={<Falcon9 id={27} />}
+          />
+        </Routes>
+        <br />
+        <br />
+        <Footer />
+      </main>
+    </>
   );
 }
 
