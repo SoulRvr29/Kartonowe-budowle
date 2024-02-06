7e884eb94a35b78acb2af5a6ca04dd611f120564
Paweł Chudecki <pawelc29@interia.pl>
2024-02-06 14:36:42 +0100

    new article - oświetlenie

--- src/techSections/Oświetlenie.jsx
+++ src/techSections/Oświetlenie.jsx
@@ -1,11 +1,79 @@
 import TechHeader from "../components/TechHeader";
+import ArticleGallery from "../components/ArticleGallery";
+import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 const Article = () => {
+  ///////////////////////// PHOTOS SCRIPTS /////////////////////////
+  const [photoIndex, setPhotoIndex] = useState(null);
+  const photosSrc = [
+    "techSections/2121221.jpg",
+    "/techSections/5885548.png",
+    "/techSections/SS850678 copy.jpg",
+    "/techSections/SS852693.JPG",
+  ];
+  const photosTitle = [
+    "porównanie kilku rodzajów diod LED",
+    "",
+    "",
+    "dwie diody SMD 0805 w latarni ulicznej",
+  ];
+  const photoClickHandler = (e) => {
+    setPhotoIndex(photosSrc.indexOf(e.target.getAttribute("src")));
+    document.querySelector("body").classList.add("site-overflow");
+  };
+  /////////////////////////////////////////////////////////////////
   return (
     <>
-      <TechHeader title="Oświetlenie modeli" />
+      <ArticleGallery
+        photosSrc={photosSrc}
+        photosTitle={photosTitle}
+        photoIndex={photoIndex}
+        setPhotoIndex={setPhotoIndex}
+      />
+      <TechHeader title="Oświetlenie w modelach" />
       <article>
         <p>
+          W swoich modelach do wykonania oświetlenia używam różnego rodzaju diod
+          LED w większości białych o ciepłej barwie światła. Można je podzielić
+          na dwa typy - standardowe i SMD.
+        </p>
+        <LazyLoadImage
+          className="mx-auto"
+          src={photosSrc[0]}
+          alt={photosTitle[0]}
+          title={photosTitle[0]}
+          onClick={(e) => {
+            photoClickHandler(e);
+          }}
+        />
+        <p>
+          <span className="font-bold">Diody standardowe: </span>
+          wąski kąt świecenia (kilkadziesiąt stopni), dają ładny efekt snopów
+          światła przy oświetlaniu ścian, ale są trudne w montażu. Pod takie
+          diody trzeba przygotować otwory i odpowiednio dużo miejsca, np. dioda
+          LED 3mm przy zagiętych nóżkach ma 6mm wysokości. Do podświetlania
+          ścian używam diod o średnicy 3mm, w latarniach morskich o średnicy
+          5mm.
+        </p>
+        <p>
+          <span className="font-bold">Diody SMD: </span>
+          szeroki kąt świecenia (do 160 stopni), są łatwe w montażu. Mają dużo
+          różnych rozmiarów, ja używam zazwyczaj SMD 3528 (3.5mm na 2.8mm), a do
+          mniejszych elementów jak np. światło latarnii ulicznej przy zamku w
+          Będzinie SMD 0805. Do takich diod przylutowuję przewody z cienkiego
+          drutu nawojowego (ok. 0.2mm).
+        </p>
+        <LazyLoadImage
+          className="mx-auto"
+          src={photosSrc[3]}
+          alt={photosTitle[3]}
+          title={photosTitle[3]}
+          onClick={(e) => {
+            photoClickHandler(e);
+          }}
+        />
+        {/* <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
           eveniet cum, nulla tempora deleniti ad amet architecto eos aperiam
           quod eum cupiditate ex sequi, harum ratione quidem aliquam deserunt
@@ -16,16 +84,17 @@ const Article = () => {
           iure reiciendis eum, totam officiis magni rerum sit. Voluptates
           voluptas veritatis nemo beatae consequuntur numquam vel, nesciunt
           dicta distinctio, dolorem doloremque sed quos totam. Quibusdam fugit
-          consequuntur accusamus. Dolore, repellendus nam, debitis reprehenderit
-          ut laborum, est magnam saepe obcaecati eos veniam neque aliquam?
-          Architecto eum ab assumenda nam. Modi vel fugit dolorum illo
-          doloremque! Voluptates numquam, illum reiciendis esse atque ipsa
-          quibusdam cumque iusto blanditiis architecto autem. Asperiores
-          architecto expedita consectetur exercitationem, nihil illo modi aut
-          tempore adipisci pariatur voluptatem eaque dolores sunt quasi. Eum
-          nemo voluptatum autem facere ab debitis quidem et vel recusandae
-          voluptatem illo quas deserunt error id, assumenda aperiam voluptas
-          neque placeat sit natus. Excepturi magni in omnis molestias facilis.
+          consequuntur accusamus.{" "}
+          Dolore, repellendus nam, debitis reprehenderit ut laborum, est magnam
+          saepe obcaecati eos veniam neque aliquam? Architecto eum ab assumenda
+          nam. Modi vel fugit dolorum illo doloremque! Voluptates numquam, illum
+          reiciendis esse atque ipsa quibusdam cumque iusto blanditiis
+          architecto autem. Asperiores architecto expedita consectetur
+          exercitationem, nihil illo modi aut tempore adipisci pariatur
+          voluptatem eaque dolores sunt quasi. Eum nemo voluptatum autem facere
+          ab debitis quidem et vel recusandae voluptatem illo quas deserunt
+          error id, assumenda aperiam voluptas neque placeat sit natus.
+          Excepturi magni in omnis molestias facilis.
         </p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid quas
@@ -74,7 +143,7 @@ const Article = () => {
           voluptatum animi laboriosam voluptas odio, possimus molestias modi
           dolore dolorum iste! Enim ratione aspernatur quisquam nihil. Cumque
           nisi numquam nam accusamus molestias enim.
-        </p>
+        </p> */}
       </article>
     </>
   );
