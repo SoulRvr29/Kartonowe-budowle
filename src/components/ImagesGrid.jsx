2f2b599baf3d1c5b224f17a842fc4485f76635b2
Paweł Chudecki <pawelc29@interia.pl>
2024-03-11 16:10:01 +0100

    ImagesGrid component update, home update

--- src/components/ImagesGrid.jsx
+++ src/components/ImagesGrid.jsx
@@ -1,67 +1,127 @@
 import { NavLink } from "react-router-dom";
-import data from "../data/home-photos-data.json";
+import modelsData from "../data/models-data.json";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useState, useEffect } from "react";
 
 const ImagesGrid = () => {
+  const [data, setData] = useState([]);
+
   const randomArr = (length) => {
     let arr = [];
     while (arr.length < length) {
-      let nr = Math.floor(Math.random() * length + 1);
-      if (!arr.includes(nr)) {
-        arr.push(nr);
-      }
+      let randomArrNr = Math.floor(Math.random() * length + 1);
+      if (!arr.includes(randomArrNr)) arr.push(randomArrNr);
     }
     return arr;
   };
 
-  const setSrc = (order) => {
-    let srcArr = [];
-    for (let x = 0; x <= order.length - 1; x++) {
-      if (order[x] < 10) srcArr.push(`models/home/home-photo0${order[x]}.jpg`);
-      else srcArr.push(`models/home/home-photo${order[x]}.jpg`);
-    }
-    return srcArr;
-  };
-  const order = randomArr(data.length);
-  const photoSrc = setSrc(order);
+  const randomData = (length) => {
+    let arr = randomArr(length);
+    let result = [];
 
-  // let order = [];
-  // for (let x = 1; x <= 30; x++) {
-  //   order.push(x);
-  // }
-  // console.log(order);
+    for (let x = 0; x < length; x++) {
+      let nr = arr[x];
+      let photo = modelsData[nr].Galeria;
+
+      // Galleria Sandomierz fix
+      if (photo === undefined) {
+        let undRandomNr = Math.floor(Math.random() * 2 + 1);
+        console.log(undRandomNr);
+        undRandomNr == 1
+          ? (photo = modelsData[nr]["Galeria - Ratusz"])
+          : (photo = modelsData[nr]["Galeria - Brama"]);
+      }
+
+      // check if there are gallery sections
+      if (Object.entries(photo).length > 1) {
+        let randomGalleryType = Math.floor(
+          Math.random() * Object.entries(photo).length
+        );
+        let randomPhotoNr = Math.floor(
+          Math.random() * Object.entries(photo)[randomGalleryType][1].quantity +
+            1
+        );
+        if (randomPhotoNr < 10) randomPhotoNr = `0${randomPhotoNr}`;
+
+        photo =
+          Object.entries(photo)[randomGalleryType][1].thumb +
+          randomPhotoNr +
+          ".jpg";
+      }
+      // if there is single gallery section
+      else {
+        let randomPhotoNr = Math.floor(
+          Math.random() * photo[Object.keys(photo)[0]].quantity + 1
+        );
+        if (randomPhotoNr < 10) randomPhotoNr = `0${randomPhotoNr}`;
+        photo = photo[Object.keys(photo)[0]].thumb + randomPhotoNr + ".jpg";
+      }
+
+      let name = modelsData[arr[x]].name;
+      let link = modelsData[arr[x]].link;
+
+      result.push({ src: photo, name: name, link: link });
+    }
+
+    return result;
+  };
+
+  useEffect(() => {
+    setData(randomData(modelsData.length - 1));
+  }, []);
 
   return (
-    <div className="flex flex-wrap gap-2 justify-center ">
-      {photoSrc.map((src, index) => (
-        <div
-          key={index}
-          className=" relative border-2 border-bkg border-opacity-50 hover:border-white hover:drop-shadow-[0_0_7px_rgba(255,255,255,0.5)] "
+    <>
+      <h4 className="losowe-header sticky top-0 z-10 flex justify-center items-center gap-4 mb-2 bg-bkg-light dark:bg-bkg">
+        Losowe zdjęcia{" "}
+        <button
+          className="absolute top-2 right-0 hover:scale-110 transition-transform fill-text-dark dark:fill-text-light header-icon hover:fill-accent-4 dark:hover:fill-accent-2 shuffle-icon"
+          title="losuj"
+          onClick={() => {
+            setData(randomData(modelsData.length - 1));
+            document.querySelector(".losowe-container").scrollIntoView();
+          }}
         >
-          <NavLink
-            to={data[order[index] - 1].link}
-            onClick={() =>
-              document.querySelector(".article-header").scrollIntoView()
-            }
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            height="1em"
           >
-            <div className="photo-container">
-              <LazyLoadImage
-                className=" gallery-thumb hover:cursor-pointer max-h-[12rem] w-auto max-sm:max-h-none max-sm:h-auto max-sm:w-full"
-                width={200}
-                height={200}
-                src={src}
-                alt={data[order[index] - 1].name}
-              />
-              <div className="photo-div invisible absolute grid justify-center items-end top-0 left-0 w-full h-full ">
-                <div className="w-full absolute px-1 bg-black  bg-opacity-50  text-md font-bold text-center text-white py-1">
-                  {data[order[index] - 1].name}
+            <path d="M105.1 202.6c7.7-21.8 20.2-42.3 37.8-59.8c62.5-62.5 163.8-62.5 226.3 0L386.3 160H352c-17.7 0-32 14.3-32 32s14.3 32 32 32H463.5c0 0 0 0 0 0h.4c17.7 0 32-14.3 32-32V80c0-17.7-14.3-32-32-32s-32 14.3-32 32v35.2L414.4 97.6c-87.5-87.5-229.3-87.5-316.8 0C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5zM39 289.3c-5 1.5-9.8 4.2-13.7 8.2c-4 4-6.7 8.8-8.1 14c-.3 1.2-.6 2.5-.8 3.8c-.3 1.7-.4 3.4-.4 5.1V432c0 17.7 14.3 32 32 32s32-14.3 32-32V396.9l17.6 17.5 0 0c87.5 87.4 229.3 87.4 316.7 0c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.5 62.5-163.8 62.5-226.3 0l-.1-.1L125.6 352H160c17.7 0 32-14.3 32-32s-14.3-32-32-32H48.4c-1.6 0-3.2 .1-4.8 .3s-3.1 .5-4.6 1z" />
+          </svg>
+        </button>
+      </h4>
+      <div className="flex flex-wrap gap-2 justify-center ">
+        {data.map((item, index) => (
+          <div
+            key={index}
+            className="random-img relative border-2 border-bkg border-opacity-50 hover:border-white hover:drop-shadow-[0_0_7px_rgba(255,255,255,0.5)] "
+          >
+            <NavLink
+              to={item.link}
+              onClick={() =>
+                document.querySelector(".article-header").scrollIntoView()
+              }
+            >
+              <div className="photo-container">
+                <LazyLoadImage
+                  className=" gallery-thumb hover:cursor-pointer max-h-[15rem] w-auto max-[400px]:max-h-full max-[400px]:max-w-full "
+                  width={200}
+                  height={200}
+                  src={item.src}
+                  alt={item.name}
+                />
+                <div className="photo-div invisible absolute grid justify-center items-end top-0 left-0 w-full h-full ">
+                  <div className="w-full absolute px-1 bg-black  bg-opacity-50 font-bold break-words break text-center text-white py-1">
+                    {item.name}
+                  </div>
                 </div>
               </div>
-            </div>
-          </NavLink>
-        </div>
-      ))}
-    </div>
+            </NavLink>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -38,11 +38,11 @@ const Article = () => {
       >
         <hr className="grad-hr" />
 
-        <header className="article-header bg-opacity-20 bg-white dark:bg-text-dark dark:bg-opacity-30  bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.2)] dark:via-[rgba(0,122,204,0.15)] to-transparent z-10 px-8 max-sm:px-4 py-4 max-sm:py-0 max-sm:pb-2 ">
+        <header className="article-header bg-opacity-20 bg-white dark:bg-text-dark dark:bg-opacity-30  bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.2)] dark:via-[rgba(0,122,204,0.15)] to-transparent z-10 px-8 max-sm:px-4 py-4 pb-0 max-sm:py-0 max-sm:pb-2 ">
           {/* <h2 className="max-w-3xl font-bold text-xl mx-auto text-center max-sm:text-sm m-2 droh2-shadow-sm dark:text-text-light text-text-dark">
             Witaj na stronie głównej.
           </h2> */}
-          <h4>Modele w budowie:</h4>
+          <h4>Aktualnie w budowie:</h4>
           <p>
             <b>
               <NavLink
@@ -78,12 +78,9 @@ const Article = () => {
             <li>Samolot P51-B Mustang</li>
           </ul>
         </header>
-        <hr className="grad-hr" />
+        {/* <hr className="grad-hr" /> */}
       </PhotoProvider>
-      <div className="m-8 my-4 max-sm:mb-2">
-        {/* <h4 className="max-sm:text-base max-[350px]:text-xs mx-auto text-center mb-4">
-          Losowe zdjęcia z różnych modeli:
-        </h4> */}
+      <div className="losowe-container m-8 max-sm:m-4 mb-4 max-sm:mb-2 relative">
         <ImagesGrid />
       </div>
     </>
