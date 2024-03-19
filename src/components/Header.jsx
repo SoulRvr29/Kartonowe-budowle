e009112f5883b7deb3db22204217077ce8b659f3
Paweł Chudecki <pawelc29@interia.pl>
2024-03-19 16:23:35 +0100

    header component svg icons replace to react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Settings from "./Settings";
 import Timer from "../components/Timer";
+import { FaHome, FaLightbulb, FaInfoCircle } from "react-icons/fa";
+import { FaGear } from "react-icons/fa6";
 
 function App({ bannerState, setBannerState, overlap, setOverlap }) {
   if (localStorage.getItem("darkMode") == null)
@@ -82,16 +84,11 @@ function App({ bannerState, setBannerState, overlap, setOverlap }) {
         <div className="flex gap-2 z-10 ml-4 -mb-1 max-[560px]:mb-1 max-[560px]:gap-4 ">
           <button className="mb-1">
             <Link to="/" title="strona główna">
-              {/* <Link to="/" title="strona główna" reloadDocument> */}
-              <svg
+              <FaHome
                 className="header-icon  transition-all"
-                height="1.1em"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 576 512"
-                fill="var(--icon-gray)"
-              >
-                <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
-              </svg>
+                size={22}
+                color="var(--icon-gray)"
+              />
             </Link>
           </button>
           <Timer />
@@ -117,76 +114,46 @@ function App({ bannerState, setBannerState, overlap, setOverlap }) {
             <div>l</div>
             <div>e</div>
           </h1>
-          {/* <h1 className="text-accent dark:brightness-100 drop-shadow-[1px_1px_0px_rgba(0,0,0,0.8)] brightness-90 text-header font-bold uppercase p-1 max-md:p-0 max-[330px]:text-center max-[330px]:leading-5 max-[330px]:p-1 max-[560px]:text-[7vw] max-[560px]:-mb-1">
-            Kartonowe &nbsp;budowle
-          </h1> */}
         </div>
         {/* right side */}
         <div className="flex gap-2 z-10 mr-4 -mb-1 max-[560px]:mb-1 max-[560px]:gap-4">
-          {/* gear icon */}
-          <button title="ustawienia">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-              height="1.1em"
-              className="header-icon hover:brightness-150 transition-all"
-              fill="var(--icon-gray)"
-              onClick={() => setSettingsState(!settingsState)}
-            >
-              <path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z" />
-            </svg>
+          <button
+            title="ustawienia"
+            onClick={() => setSettingsState(!settingsState)}
+          >
+            <FaGear
+              className="header-icon transition-all"
+              size={18}
+              color="var(--icon-gray)"
+            />
           </button>
-          {/* lightbulb icon */}
           <button
             title="kolorystyka"
             onClick={() => {
               modeChange();
             }}
           >
-            <svg
+            {" "}
+            <FaLightbulb
+              size={18}
+              color="var(--icon-gray)"
               className={
                 darkMode
-                  ? "header-icon hover:brightness-150 drop-shadow-[0_-2px_7px_white]transition-color "
-                  : "header-icon hover:brightness-150 drop-shadow-[0_-2px_6px_white] fill-white transition-color  "
+                  ? "header-icon drop-shadow-[0_-2px_7px_white]transition-color"
+                  : "header-icon drop-shadow-[0_-2px_6px_white] fill-white transition-color"
               }
-              xmlns="http://www.w3.org/2000/svg"
-              height="1.1em"
-              viewBox="0 0 384 512"
-              fill="var(--icon-gray)"
-            >
-              <path d="M272 384c9.6-31.9 29.5-59.1 49.2-86.2l0 0c5.2-7.1 10.4-14.2 15.4-21.4c19.8-28.5 31.4-63 31.4-100.3C368 78.8 289.2 0 192 0S16 78.8 16 176c0 37.3 11.6 71.9 31.4 100.3c5 7.2 10.2 14.3 15.4 21.4l0 0c19.8 27.1 39.7 54.4 49.2 86.2H272zM192 512c44.2 0 80-35.8 80-80V416H112v16c0 44.2 35.8 80 80 80zM112 176c0 8.8-7.2 16-16 16s-16-7.2-16-16c0-61.9 50.1-112 112-112c8.8 0 16 7.2 16 16s-7.2 16-16 16c-44.2 0-80 35.8-80 80z" />
-            </svg>
+            />
           </button>
-          {/* mail icon */}
-          {/* <a title="wyślij mail" href="mailto:pawelc29@interia.pl">
-            <svg
-              className="header-icon hover:brightness-150 transition-all "
-              xmlns="http://www.w3.org/2000/svg"
-              height="1.25em"
-              viewBox="0 0 512 512"
-              fill="var(--icon-gray)"
-            >
-              <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
-            </svg>
-          </a> */}
-          {/* about icon */}
-          <button
+          <Link
+            to="/About"
+            title="o autorze"
+            className="header-icon transition-all"
             onClick={() => {
               document.querySelector(".article-header").scrollIntoView();
             }}
           >
-            <Link to="/About" title="o autorze">
-              <svg
-                className="header-icon hover:brightness-150 transition-all "
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                height="1.1em"
-                fill="var(--icon-gray)"
-              >
-                <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
-              </svg>
-            </Link>
-          </button>
+            <FaInfoCircle size={18} color="var(--icon-gray)" />
+          </Link>
         </div>
       </header>
       {settingsState && (
