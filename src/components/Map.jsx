519dc67f8d3045a644df14f4a1dc2beddd465e72
Paweł Chudecki <pawelc29@interia.pl>
2025-01-23 16:27:40 +0100

    map active on button click

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -11,6 +11,7 @@ const map = ({ id, position }) => {
   const [zoom, setZoom] = useState(17);
   const [center, setcenter] = useState([...position]);
   const polandMap = [52, 19];
+  const [mapActive, setMapActive] = useState(false);
 
   return (
     <div
@@ -20,14 +21,29 @@ const map = ({ id, position }) => {
           : "w-full max-w-sm relative h-[20rem] "
       }
     >
+      {!mapActive && (
+        <div className="bg-black grid place-content-center text-xl bg-opacity-30 absolute z-10 top-0 left-0 w-full h-full ">
+          <button
+            onClick={() => {
+              setMapActive(true);
+            }}
+            className="text-white drop-shadow-[0_0_4px_black] bg-black bg-opacity-20 px-3 py-1 pb-[6px] upp rounded-xl hover:text- hover:border-2"
+          >
+            Kliknij by aktywować
+          </button>
+        </div>
+      )}
       <div className="z-10 absolute right-3 top-3 flex flex-col  dark:brightness-75 dark:contrast-150">
         {/* map size button */}
         <button
           title={mapSize ? "małe okno" : "duże okno"}
           onClick={() => {
-            setMapSize(!mapSize);
+            mapActive && setMapSize(!mapSize);
           }}
-          className="map-btn border-2 border-b rounded-t-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100"
+          className={
+            "map-btn border-2 border-b rounded-t-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100" +
+            (!mapActive && " brightness-75 hover:bg-white hover:cursor-default")
+          }
         >
           {mapSize ? (
             <PiArrowsInBold size={18} color="black" className="scale-125" />
@@ -38,16 +54,20 @@ const map = ({ id, position }) => {
         {/* map zoom button */}
         <button
           title={zoom === 17 ? "mapa Polski" : "mapa szczegółowa"}
-          className="border-2 border-t-0 rounded-b-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100 w-8 flex justify-center h-8"
+          className={
+            "border-2 border-t-0 rounded-b-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100 w-8 flex justify-center h-8" +
+            (!mapActive && " brightness-75")
+          }
           onClick={() => {
-            if (zoom === 17) {
-              if (!mapSize) setZoom(5);
-              else setZoom(6);
-              setcenter(polandMap);
-            } else {
-              setZoom(17);
-              setcenter([...position]);
-            }
+            if (mapActive)
+              if (zoom === 17) {
+                if (!mapSize) setZoom(5);
+                else setZoom(6);
+                setcenter(polandMap);
+              } else {
+                setZoom(17);
+                setcenter([...position]);
+              }
           }}
         >
           {zoom === 17 ? (
--- src/components/MapAll.jsx
+++ src/components/MapAll.jsx
@@ -2,27 +2,44 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import { NavLink } from "react-router-dom";
 import modelsData from "../data/models-data.json";
 import { PiArrowsOutBold, PiArrowsInBold } from "react-icons/pi";
+import { useState } from "react";
 
 const map = ({ mapSize, setMapSize }) => {
   const polandMap = [52.4, 19];
   const zoom = 6;
+  const [mapActive, setMapActive] = useState(false);
 
   return (
     <div
       className={
         mapSize
-          ? "w-full mx-auto my-4 relative h-[50rem] max-sm:h-[40rem] "
-          : "w-full mx-auto my-4 max-w-lg relative h-[30rem] max-sm:h-[24rem]"
+          ? "w-full border-2 rounded-xl border-accent overflow-hidden mx-auto my-4 relative h-[50rem] max-sm:h-[40rem] "
+          : "w-full border-2 rounded-xl border-accent overflow-hidden mx-auto my-4 max-w-lg relative h-[30rem] max-sm:h-[24rem]"
       }
     >
+      {!mapActive && (
+        <div className="bg-black grid place-content-center text-xl bg-opacity-30 absolute z-10 top-0 left-0 w-full h-full ">
+          <button
+            onClick={() => {
+              setMapActive(true);
+            }}
+            className="text-white drop-shadow-[0_0_4px_black] bg-black bg-opacity-20 px-3 py-1 pb-[6px] upp rounded-xl hover:text- hover:border-2"
+          >
+            Kliknij by aktywować
+          </button>
+        </div>
+      )}
       <div className="z-10 absolute right-3 top-3 flex flex-col  ">
         {/* map size button */}
         <button
           title={mapSize ? "małe okno" : "duże okno"}
           onClick={() => {
-            setMapSize(!mapSize);
+            mapActive && setMapSize(!mapSize);
           }}
-          className="map-btn border-2 border-b rounded-t-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100"
+          className={
+            "map-btn border-2 border-b rounded-t-[3px] bg-white border-gray-400 p-[5px] hover:bg-gray-100" +
+            (!mapActive && " brightness-75")
+          }
         >
           {mapSize ? (
             <PiArrowsInBold size={18} color="black" className="scale-125" />
@@ -37,7 +54,7 @@ const map = ({ mapSize, setMapSize }) => {
         zoom={zoom}
         scrollWheelZoom={true}
         zoomControl={true}
-        className="map h-full border-2 rounded-xl border-accent drop-shadow-[5px_5px_1px_rgba(0,0,0,0.1)] dark:drop-shadow-[0px_0px_20px_rgba(0,122,204,0.2)]"
+        className="map h-full  drop-shadow-[5px_5px_1px_rgba(0,0,0,0.1)] dark:drop-shadow-[0px_0px_20px_rgba(0,122,204,0.2)]"
       >
         <TileLayer
           attribution='&copy;
