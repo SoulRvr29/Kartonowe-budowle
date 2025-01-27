82b3479b53b507d5312ef659f458fd52598fda90
Paweł Chudecki <pawelc29@interia.pl>
2025-01-27 12:03:12 +0100

    new search bar

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,7 +2,7 @@ import Card from "./Card.jsx";
 import modelsData from "../data/models-data.json";
 import { useState, useRef } from "react";
 import { FaCaretLeft, FaCaretRight, FaArrowsLeftRight } from "react-icons/fa6";
-import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowUp, IoMdSearch } from "react-icons/io";
 
 const Nav_li = ({ name, activeList, setActiveList, completeData, setData }) => {
   return (
@@ -45,8 +45,18 @@ const Nav = ({ overlap, setOverlap, headerSticky }) => {
   const [navState, setNavState] = useState(true);
   const [data, setData] = useState(completeData);
   const [activeList, setActiveList] = useState("wszystkie modele");
+  const [search, setSearch] = useState("");
+  const [searchActive, setSearchActive] = useState(false);
 
   const navWidth = useRef(null);
+
+  function removePLChars(str) {
+    return str
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/ł/g, "l") // Zamienia 'ł' na 'l'
+      .replace(/Ł/g, "L"); // Zamienia 'Ł' na 'L';
+  }
 
   return (
     <div className="relative grid select-none z-10 bg-gradient-to-b from-transparent via-[rgba(255,255,255,0.4)] dark:via-text-dark to-transparent ">
@@ -58,6 +68,46 @@ const Nav = ({ overlap, setOverlap, headerSticky }) => {
           {data.length}
         </div>
       )}
+      {/* SEARCH BAR */}
+      <div className="mx-8 max-sm:mx-0 relative">
+        {!searchActive && (
+          <IoMdSearch
+            size={20}
+            className="absolute left-[calc(50%-2.6rem)] top-[5px] opacity-80 pointer-events-none"
+          />
+        )}
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Szukaj..."
+          className="w-full px-1 bg-white dark:bg-opacity-5 bg-opacity-20 text-lg text-text-dark dark:text-white placeholder:text-black placeholder:-ml-6 dark:placeholder:text-white dark:placeholder:opacity-50 placeholder:text-opacity-50 focus:placeholder:invisible text-center font-semibold focus:bg-opacity-30 dark:focus:bg-opacity-10 focus:outline-none"
+          value={search}
+          onFocus={() => {
+            setSearch("");
+            setSearchActive(true);
+            setData(completeData);
+            setActiveList("");
+          }}
+          onBlur={() => {
+            setSearch("");
+            setSearchActive(false);
+          }}
+          onChange={(e) => {
+            const text = e.target.value;
+            const data = modelsData;
+            setSearch(text);
+            const filteredData = data.filter((item) => {
+              return (
+                removePLChars(item.name.toLowerCase()).indexOf(
+                  removePLChars(text.toLowerCase())
+                ) !== -1
+              );
+            });
+            setData(filteredData);
+          }}
+        />
+      </div>
       {/* CATEGORIES LIST */}
       <div className={navState ? "relative mx-8 max-sm:mx-0" : "hidden"}>
         <ul className="flex mt-1 max-sm:text-sm flex-wrap gap-x-[0.4rem]  max-[700px]:gap-[0.2rem] w-full text-center justify-center nav-clamp tracking-wide dark:text-text-light text-bkg bg-opacity-30 font-semibold px-6 max-sm:px-4 text-lg max-sm:gap-1 ">
@@ -147,7 +197,7 @@ const Nav = ({ overlap, setOverlap, headerSticky }) => {
             ref={navWidth}
             className={
               navState === true
-                ? "cards-container nav flex relative py-1 transition-all max-md:gap-4 "
+                ? "cards-container nav flex h-[190px] max-sm:h-[160px] relative py-1 transition-all max-md:gap-4 "
                 : "cards-container nav hidden"
             }
           >
@@ -159,6 +209,11 @@ const Nav = ({ overlap, setOverlap, headerSticky }) => {
                 headerSticky={headerSticky}
               />
             ))}
+            {data.length === 0 && (
+              <div className="grid place-content-center text-xl">
+                Brak wyników
+              </div>
+            )}
           </div>
         </section>
       </nav>
