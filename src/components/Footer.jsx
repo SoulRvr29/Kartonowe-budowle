b64aa60ee73f0195f6df23fcc1b14a17d24b89a9
Paweł Chudecki <pawelc29@interia.pl>
2025-02-19 11:03:46 +0100

    auto actual year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import { IoIosMail } from "react-icons/io";
 
 const Footer = () => {
+  const actualYear = new Date().getFullYear();
+
   return (
     <footer className="absolute w-full bottom-0 text-center z-10">
       <div className="relative grad-hr drop-shadow-[0_1px_0_#666] bottom-[1px]" />
       <div className="flex justify-center items-center">
         <h2 className="p-1 align-baseline font-medium">
-          Paweł Chudecki &copy; 2023-2025
+          Paweł Chudecki &copy; 2023-{actualYear}
         </h2>
 
         <a title="wyślij mail" href="mailto:pawelc29@interia.pl">
